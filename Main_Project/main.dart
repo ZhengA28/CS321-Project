84bfd4a9854846c841e11f2710ea1f7ee9d14ad0
ZhengA28 <124634085+ZhengA28@users.noreply.github.com>
2024-10-19 01:45:04 -0400

    Add files via upload

--- Main_Project/main.dart
+++ Main_Project/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial_project/pages/home.dart';
+import 'package:tutorial_project/pages/preset.dart';
+import 'package:tutorial_project/pages/user_profile.dart';
+import 'package:tutorial_project/pages/settings.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,120 +17,16 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold( //Contains content on screen
-        backgroundColor: Colors.black54,
 
-        //Creates a bar at top of app
-        appBar: AppBar(
-          title: Center(
-            child: Text("FlexTimer",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
+      home: const HomePage(), //Initial Page on app startup
+      theme: ThemeData(primarySwatch: Colors.teal), //Set theme color of app
 
-          backgroundColor: Colors.teal[700],
-          //elevation: 0, //If elevation set to 0, removes app bar shadow
-          leading: Icon(Icons.menu),  //Creates Icon on left hand side
-          actions: [IconButton(onPressed: null, icon: Icon(Icons.add, color: Colors.black54))],  //Create icon on right hand side
-        ),
-
-        /**
-         * body: Column - Creates columns containing multiple children vertically
-         * body: Row - Creates rows containing multiple children horizontally
-         * body: Listview - Similar to Column/Row but allows scrolling through screen if children size overflows the screen
-         * body: Stack - stacks children on top of one another
-         */
-        /*body: ListView(
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //Aligns main axis of children accordingly
-          //crossAxisAlignment: CrossAxisAlignment.start, //Aligns cross axis of children accordingly
-          scrollDirection: Axis.vertical, //Specify which direction to scroll when body: Listview
-          children: [
-            //1st Box
-            Expanded( //Automatically resize child to fill screen
-              flex: 2,  //Specifcy size of child compared to other child (ex. 2 = double in size)
-              child: Container(
-                  height: 500,
-                  color: Colors.teal[600],
-                ),
-            ),
-
-            //2nd Box
-            Expanded( //Automatically resize child to fill screen
-              child: Container(
-                height: 300,
-                color: Colors.teal[400],
-              ),
-            ),
-            //3rd Box
-            Expanded( //Automatically resize child to fill screen
-              child: Container(
-                height: 200,
-                color: Colors.teal[200],
-              ),
-            ),
-          ]
-
-        ),*/
-
-        /**
-         * ListView.builder or GridView.builder can be used to create instances on demand
-         */
-        /*body: ListView.builder(
-            itemCount: names.length,  //Specify how mnay instances to create
-            itemBuilder: (context, index) => ListTile(
-              title: Text(names[index]),
-            )
-        ),*/
-        body: GridView.builder(
-            itemCount: 20,  //SPecify how many instances to create
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4), //Specify how many grid in each row
-            itemBuilder: (context, index) => Container(
-              color: Colors.teal[600],
-              margin: EdgeInsets.all(2),  //Specify spacing between each container
-              child: IconButton(onPressed:null, icon: Icon(Icons.access_time, color: Colors.black, size: 65)),
-            )
-        ),
-
-        //Create body containing a centered widget
-        /*body: Center (  //Centers containing widgets
-          child: Container(
-            height: 300,
-            width: 300,
-
-            decoration: BoxDecoration(
-              color: Colors.teal[700],
-              borderRadius: BorderRadius.circular(20),  //Curves the container edge
-            ),
-
-            padding: EdgeInsets.all(50),  //Creates padding for text
-
-            //GestureDetector for detecting screen taps
-            /*child: GestureDetector(
-              onTap: (){
-                time++;
-                print("Time: $time");
-              },
-              child: IconButton(onPressed: null, icon: Icon(Icons.access_time, color: Colors.white, size: 65)),
-            ),*/
-
-            //Creating Text child
-            /*child: Text(
-                "Welcome to Flextimer",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-              ),
-            ),*/
-
-          ),
-        ),*/
-      ),
+      routes:{  //List of pages for navigation
+        '/homepage' : (context) => const HomePage(),
+        'presetpage' : (context) => const PresetPage(),
+        'profilepage' : (context) => const ProfilePage(),
+        'settingspage' : (context) => const SettingsPage(),
+      }
     );
-
   }
 }
